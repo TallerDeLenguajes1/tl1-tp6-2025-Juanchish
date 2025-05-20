79578ac5853ff88282873543659e2ece79a601aa
juan <jbcb_09@hotmail.com>
2025-05-20 14:47:03 -0300

    ejercicio 1

---
+++ proyecto 1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);*/
+
+
+Console.WriteLine("Ingrese un número:");
+string? num = Console.ReadLine();
+int i;
+bool result = int.TryParse(num,out i);
+
+if (result)
+{
+    Console.WriteLine("El numero invertido es:");
+    while (i > 0)
+    {
+        Console.Write(i % 10);
+        i /= 10;
+    }
+} else
+{
+    Console.WriteLine("El valor ingresado no es un número");
+}
+
